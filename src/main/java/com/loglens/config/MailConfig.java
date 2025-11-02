a072e7a7e6a865f9c78a7171e9e85aeb7c8778c3
amarkrsoni <35839699+amarkrsoni@users.noreply.github.com>
2025-11-02 18:43:53 +0530

    Updated MailConfig.java

--- src/main/java/com/loglens/config/MailConfig.java
+++ src/main/java/com/loglens/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.loglens.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,40 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String smtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
+    private String starttlsEnable;
+
+    @Value("${spring.mail.properties.mail.debug:false}")
+    private String debug;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("kramarsoni@gmail.com");
-        mailSender.setPassword("lxwrmfcljgjacnrp"); // <-- Replace with your Gmail app password
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", starttlsEnable);
+        props.put("mail.debug", debug);
 
         return mailSender;
     }
